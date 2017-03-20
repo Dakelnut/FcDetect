55cb6cb51937c9c0662a1bd9e863dda5648d464f
Dakelnut <dakelnat@gmail.com>
2017-03-21 01:37:17 +0300

    Next steps

--- Scaning_frame_0.1.py
+++ Scaning_frame_0.1.py
@@ -1,7 +1,8 @@
 from PIL import Image
 import cv2
 from math import floor
-
+import numpy as np
+from time import sleep
 import AdaBoost_test_01
 import random
 import time
@@ -66,7 +67,7 @@ class Frame:
         
         print("List of frame sizes:")
         print(self.list_of_frame_sizes)
-        self.list_of_frame_sizes = self.list_of_frame_sizes[0:2]
+        self.list_of_frame_sizes = self.list_of_frame_sizes[3:-10]
         self.list_of_frame_sizes = iter(self.list_of_frame_sizes)
         self.scaning(self.image)
 
@@ -94,29 +95,47 @@ class Frame:
             self.counter = 0
             print("Size=",self.size)
             distance_from_centre = self.size[0] // 2
+            distance_to_next_centre = round(distance_from_centre * (1/3))
         except:
             print("----------------------------------")
             print("-----------EndOfScaning-----------")
             print("----------------------------------")
             return self.copy_of_image
 
-
-
-        for i in range(len(image)):
-            for j in range(len(image[0])):
+        i = distance_from_centre
+        j = distance_from_centre
+        while i <= (len(image)-distance_from_centre-1):
+            while j <= (len(image[0])-distance_from_centre-1):
 
                 if self.checking_condition(i,j,distance_from_centre) == True:
         
                     our_frame = image[i - distance_from_centre : i + distance_from_centre + 1]
                     our_frame = [our_frame[k][j-distance_from_centre : j + distance_from_centre + 1] for k in range(len(our_frame))]
 
+                    # cv2.imshow('img', np.array(our_frame))
+                    # cv2.waitKey(5)
+                    # sleep(1)
+                    # cv2.destroyAllWindows()
+
+
                     answer =  AdaBoost_test_01.AdaBoost('n',our_frame).ans
 
+
                     if answer == True:
+                        print("Face recognised")
+
+                        cv2.imshow('img', np.array(our_frame))
+                        cv2.waitKey(3)
+                        sleep(0.5)
+                        cv2.destroyAllWindows()
 
                         for m in range(i - distance_from_centre, i + distance_from_centre + 1):
                             for n in range(j - distance_from_centre, j + distance_from_centre + 1):
                                 self.copy_of_image[m][n] = 1
+
+                j = j + distance_to_next_centre
+            j = distance_from_centre
+            i = i + distance_to_next_centre
 
         self.scaning(self.image)
 
@@ -134,37 +153,32 @@ class Frame:
     def checking_condition(self,i,j,distance_from_centre):
 
 
+        self.counter = self.counter + 1
+        if self.counter == 1:
+            self.prev_i = i
+            self.prev_j = j
+
+        zone = self.copy_of_image[i - distance_from_centre: i + distance_from_centre + 1]
 
 
-        
-        if (distance_from_centre <= i < self.height - distance_from_centre) and (distance_from_centre <= j < self.width - distance_from_centre):
-            self.counter = self.counter + 1
-            if self.counter == 1:
-                self.prev_i = i
-                self.prev_j = j
+        height_of_zone = len(zone)
 
-            zone = self.copy_of_image[i - distance_from_centre: i + distance_from_centre + 1]
+        zone = [zone[k][j - distance_from_centre: j + distance_from_centre + 1] for k in range(len(zone))]
 
 
-            height_of_zone = len(zone)
+        width_of_zone = len(zone[0])
+        num_of_pixels_in_zone = height_of_zone * width_of_zone
 
-            zone = [zone[k][j - distance_from_centre: j + distance_from_centre + 1] for k in range(len(zone))]
+        # if (abs(i - self.prev_i) >= distance_from_centre) or (abs(j - self.prev_j) >= distance_from_centre ) or self.counter == 1:
+        #     self.prev_i = i
+        #     self.prev_j = j
+        # else:
+        #     return False
 
-
-            width_of_zone = len(zone[0])
-            num_of_pixels_in_zone = height_of_zone * width_of_zone
-
-            if (abs(i - self.prev_i) >= distance_from_centre) or (abs(j - self.prev_j) >= distance_from_centre ) or self.counter == 1:
-                self.prev_i = i
-                self.prev_j = j
-            else:
-                return False
-
-            if (round(zone.count(1)/num_of_pixels_in_zone,2)*100) <= 5 :
-                return True
-            else: return False
-
+        if (round(zone.count(1)/num_of_pixels_in_zone,2)*100) <= 2 :
+            return True
         else: return False
+
 
 
 
@@ -175,6 +189,7 @@ def main():
 
 
     fr = Frame(image_grey)
+
     for i in range(len(fr.copy_of_image)):
         for j in range(len(fr.copy_of_image[0])):
             if fr.copy_of_image[i][j] == 1:
@@ -182,10 +197,11 @@ def main():
 
 
 
+
     cv2.imshow('img', image)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
-
+    cv2.imwrite("my.jpg",image)
     end = time.time()
     print("TIME:",end-start)
 
